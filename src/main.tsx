3b48872654b12bb9ef978bbb9ae4af0728fa1766
Keaton Greer <keatongreer1@gmail.com>
2024-10-30 23:18:10 -0400

    Added starter files

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import App from './App.tsx';
+import CandidateSearch from './pages/CandidateSearch.tsx';
+import SavedCandidates from './pages/SavedCandidates.tsx';
+import ErrorPage from './pages/ErrorPage.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <CandidateSearch />,
+      },
+      {
+        path: '/SavedCandidates',
+        element: <SavedCandidates />,
+      },
+    ],
+  },
+]);
+
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(<RouterProvider router={router} />);
+}
