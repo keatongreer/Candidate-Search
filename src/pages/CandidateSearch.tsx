9c9d47e09602e0fc9201bcebddb352559f04a7b6
Keaton Greer <keatongreer1@gmail.com>
2024-11-03 16:41:40 -0500

    Added initial CandidateSearch page and implemented candidate fetch

--- src/pages/CandidateSearch.tsx
+++ src/pages/CandidateSearch.tsx
@@ -1,8 +1,110 @@
-import { useState, useEffect } from 'react';
-import { searchGithub, searchGithubUser } from '../api/API';
+import { useState, useEffect } from "react";
+import { searchGithub, searchGithubUser } from "../api/API";
+import Candidate from "../interfaces/Candidate.interface";
+import { CgLayoutGrid } from "react-icons/cg";
 
 const CandidateSearch = () => {
-  return <h1>CandidateSearch</h1>;
+  const [currentCandidate, setCurrentCandidate] = useState<Candidate>({
+    login: "",
+    name: "",
+    location: "",
+    email: "",
+    company: "",
+    bio: "",
+    avatar_url: "",
+    html_url: "",
+  });
+
+  const [potentialCandidates, setPotentialCandidates] = useState<Candidate[]>(
+    []
+  );
+
+  const fetchCandidates = async () => {
+    try {
+      // get the initial list of users
+      const data = await searchGithub();
+      const candidateLogins: string[] = data.map((user: any) => user.login);
+
+      const candidateData: Candidate[] = [];
+
+      // fetch individual users and push to candidateData array
+      for (const login of candidateLogins) {
+        const user = await searchGithubUser(login);
+        candidateData.push({
+          login: user.login,
+          name: user.name || null,
+          location: user.location || null,
+          email: user.email || null,
+          company: user.company || null,
+          bio: user.bio || null,
+          avatar_url: user.avatar_url,
+          html_url: user.html_url,
+        });
+      }
+
+      // set array of potential candidates
+      setPotentialCandidates(candidateData);
+
+      // set initial current candidate
+      setCurrentCandidate(potentialCandidates[0]);
+    } catch (error) {
+      console.error("Error fetching candidate data:", error);
+    }
+  };
+
+  const acceptCandidate = () => {};
+
+  const rejectCandidate = () => {
+    // remove current candidate from the array of potential candidates
+    setPotentialCandidates(
+      potentialCandidates.splice(
+        potentialCandidates.indexOf(currentCandidate),
+        1
+      )
+    );
+
+    // set current candidate
+    setCurrentCandidate(potentialCandidates[0]);
+  };
+
+  useEffect(() => {
+    fetchCandidates();
+  }, []);
+
+  const addToSavedCandidates = () => {
+    let parsedCandidates: Candidate[] = [];
+    const storedCandidates = localStorage.getItem("filmsToWatch");
+    if (typeof storedCandidates === "string") {
+      parsedCandidates = JSON.parse(storedCandidates);
+    }
+    parsedCandidates.push(currentCandidate);
+    localStorage.setItem("savedCandidates", JSON.stringify(parsedCandidates));
+  };
+
+  return (
+    <div className="card">
+      <img src={currentCandidate.avatar_url} className="card-img-top" />
+      <div className="card-body">
+        <h5 className="card-title">
+          {currentCandidate.name} ({currentCandidate.login})
+        </h5>
+        <div className="card-text">
+          <p>Location: {currentCandidate.location || "None"}</p>
+          <p>Email: {currentCandidate.email || "None"}</p>
+          <p>Company: {currentCandidate.company || "None"}</p>
+          <p>Bio: {currentCandidate.bio || "None"}</p>
+        </div>
+      </div>
+      <div className="buttons">
+        <button className="rejectCandidate" onClick={rejectCandidate}>
+          -
+        </button>
+        <button className="acceptCandidate" onClick={acceptCandidate}>
+          +
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default CandidateSearch;
