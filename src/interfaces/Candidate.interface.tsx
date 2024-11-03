9dead1c933df6337ec3d208a3b1a54aeefac0486
Keaton Greer <keatongreer1@gmail.com>
2024-11-03 16:39:29 -0500

    Added Candidate interface

--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,11 @@
-// TODO: Create an interface for the Candidate objects returned by the API
+// Create an interface for the Candidate objects returned by the API
+export default interface Candidate {
+  login: string;
+  name: string | null;
+  location: string | null;
+  email: string | null;
+  company: string | null;
+  bio: string | null;
+  avatar_url: string;
+  html_url: string;
+}
